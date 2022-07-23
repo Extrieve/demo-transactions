b8bf812eca91e93e9c36e8b30b478fd6d739fac5
Nick <nicolas.cinera@gmail.com>
2022-07-23 17:50:38 -0500

    Update PostService.java

--- src/main/java/com/demo/demo/service/PostService.java
+++ src/main/java/com/demo/demo/service/PostService.java
@@ -2,6 +2,10 @@ package com.demo.demo.service;
 
 import com.demo.demo.entity.Post;
 import com.demo.demo.respository.PostRepository;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,7 +15,11 @@ import java.util.Collection;
 
 @Service
 @Transactional
+@Slf4j
 public class PostService implements Delegator {
+
+
+    private Logger logger = LoggerFactory.getLogger(PostService.class);
 
     @Autowired
     private PostRepository postRepository;
@@ -22,7 +30,14 @@ public class PostService implements Delegator {
     }
 
     public ResponseEntity<Post> getPostById(Long id) {
-        return ResponseEntity.ok(postRepository.getById(id));
+
+        try {
+            Post payload = postRepository.getById(id);
+            return ResponseEntity.ok(postRepository.getById(id));
+        } catch(RuntimeException e){
+            logger.error("Error: " + e.getMessage());
+            return ResponseEntity.badRequest().body(Post.builder().build());
+        }
     }
 
     public ResponseEntity<Post> createPost(Post post) {
